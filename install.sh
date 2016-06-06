915a6d5011b260e698455b542a6886aa642f0924
Andrew Duffy <root@aduffy.org>
2016-06-05 19:01:30 -0700

    Removed vim config (I'm on nvim now)

--- install.sh
+++ install.sh
@@ -11,11 +11,4 @@ echo 'Setting up symlinks...'
 # Tmux
 ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
 
-# Vim
-ln -s ${PWD}/vim ${HOME}/.vim
-ln -s ${PWD}/vimrc ${HOME}/.vimrc
-
-echo 'Installing vim plugins'
-vim -c ":PluginInstall"
-
 echo 'All done!'
