aecf17741f1c459f3b85e4563a1f7bf38ceca0e7
Andrew Duffy <aduffy@palantir.com>
2016-07-17 11:29:23 +0100

    Add + install iterm configuration dir

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 set -x
 set -e -o pipefail
 
+CONFIGS_DIR="$(pwd)"
+
 echo 'Installing the submodules...'
 git submodule init && git submodule
 
@@ -10,5 +12,8 @@ echo 'Setting up symlinks...'
 
 # Tmux
 ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
+
+# iTerm settings
+(cd $HOME; ln -s "${CONFIGS_DIR}"/iterm .iterm)
 
 echo 'All done!'
